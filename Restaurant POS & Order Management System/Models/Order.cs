5a442943efb131cdabec64f04e60edb862ce5c2a
Kshitij1ghk <kshitijwk@gmail.com>
2026-02-25 11:10:02 +0530

    created model for orderitem.cs

---
+++ Restaurant POS & Order Management System/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_POS___Order_Management_System.Models
+{
+    public class Order
+    {
+    }
+}
---
+++ Restaurant POS & Order Management System/Models/OrderItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_POS___Order_Management_System.Models
+{
+    public class OrderItem
+    {
+        public int OrderId { get; private set; }
+        
+        public int MenuItemId1 {  get; private set; }
+        public int Quantitiy {  get; private set; }
+
+        public Decimal PriceAtTimeOfOrder {  get; private set; }
+
+        public OrderItem(int orderId,int menuItemId,int quantity,decimal
+            priceAtTimeOfOrder)
+        {
+            if (orderId <= 0)
+            {
+                throw new ArgumentException("order id cantbe less than or equal to zero!");
+            }
+            if(menuItemId <= 0)
+            {
+                throw new ArgumentException("Menu Item ID cannot be less than or equal to zero");
+            }
+            if (priceAtTimeOfOrder <= 0)
+            {
+                throw new ArgumentException("order price can't be less than or equal to zero!");
+            }
+            if(quantity <= 0)
+            {
+                throw new ArgumentException("quantity can't be less than or equal to zero!")
+            }
+            orderId = OrderId;
+            MenuItemId1 = menuItemId;
+            Quantitiy = quantity;
+            PriceAtTimeOfOrder = priceAtTimeOfOrder;
+        }
+    }
+}

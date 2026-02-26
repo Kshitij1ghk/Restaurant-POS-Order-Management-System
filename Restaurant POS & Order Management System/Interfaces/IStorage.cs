59f9aa0bf10dca3a5e57b95b0be4c5f9e0fca18f
Kshitij1ghk <kshitijwk@gmail.com>
2026-02-26 15:53:46 +0530

    -created a new interface for Storage

--- Restaurant POS & Order Management System/Interfaces/IStorage.cs
+++ Restaurant POS & Order Management System/Interfaces/IStorage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Restaurant_POS___Order_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace Restaurant_POS___Order_Management_System.Interfaces
 {
-    internal interface IStorage
+    public interface IStorage
     {
+        void SaveMenuItems(List<MenuItem> menuItems);
+        void LoadMenuItems();
+
+        void SaveOrders(List<Order> orders);
+        void LoadOrders();
+
+        void SaveOrderItems(List<OrderItem> orderItems);
+        void LoadOrderItems();
+
+        void SaveTables(List<Table> tables);
+        void LoadTables();
+
+        void SaveStaff(List<Staff> staffs);
+        void LoadStaff();
+
+        void SaveReceipts(List<Receipt> receipts);
+        void LoadReceipts();
     }
 }
